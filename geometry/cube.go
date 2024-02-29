5f4d8851b105b0bb9dbcc6a713350dc71da594b4
dylanbrisco <dylanbrisco@gmail.com>
2024-02-28 20:57:45 -0500

    zero edge is not allowed

--- geometry/cube.go
+++ geometry/cube.go
@@ -1,5 +1,10 @@
 package geometry
 
-func Cube(n int) int {
-	return n * n * n
+import "errors"
+
+func Cube(n int) (int, error) {
+	if n != 0 {
+		return n * n * n, nil
+	}
+	return 0, errors.New("zero length edge is not allowed")
 }
